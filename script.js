131ae14dfba8257efd257457f74547bf3582ed27
leninblok <L_E_Makarov@mail.ru>
2025-04-29 10:28:58 +0300

    10

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ function checkBreakpoint() {
   	heading.innerHTML=desktopText
   }
 }
+
+let productCard = document.querySelector('.product-card')
+let shirt3 = document.querySelector('.shirt3')
+let shirt1 = document.querySelector('.shirt1')
+let deer = document.querySelector('.deer-img')
+
+document.addEventListener('scroll', function(event){
+  parallax(productCard, 10, -1)
+  parallax(shirt3,20,1)
+  parallax(shirt1,20,-1)
+  parallax(deer,17.5,1)
+})
+
+function parallax(element, offset, direction){
+  let rect = element.getBoundingClientRect()
+  // let progress = (window.innerHeight-rect.y)/(window.innerHeight+rect.y)
+  // progress = Math.max(-2, Math.min(2, progress));
+  let progress = 1 - rect.top / window.innerHeight;
+  progress = Math.max(0, Math.min(2, progress));
+  element.style.top = progress*offset*direction + '%'
+}
